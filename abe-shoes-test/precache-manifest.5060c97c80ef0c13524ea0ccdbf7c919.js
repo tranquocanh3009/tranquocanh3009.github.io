ca2618acdadc811ebc34f271275f5d23d0b6b2cd
Tran Quoc Anh <tranquocanh.3009@gmail.com>
2020-03-22 23:14:59 +0700

    abe-shoes-test 4

---
+++ abe-shoes-test/precache-manifest.5060c97c80ef0c13524ea0ccdbf7c919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e49f77eec7b7dd2d6110021e9533c531",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d367971a77db940c7f34",
+    "url": "./static/css/main.be355806.chunk.css"
+  },
+  {
+    "revision": "c65a99f4c855c7ba1db1",
+    "url": "./static/js/2.25ec9b8f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.25ec9b8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d367971a77db940c7f34",
+    "url": "./static/js/main.88970518.chunk.js"
+  },
+  {
+    "revision": "d62ad5bf391a31873e1e",
+    "url": "./static/js/runtime-main.125a1b0e.js"
+  }
+]);
--- abe-shoes-test/service-worker.js
+++ abe-shoes-test/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ea6430d554dd61530b561f2a526bcc8c.js"
+  "./precache-manifest.5060c97c80ef0c13524ea0ccdbf7c919.js"
 );
 
 self.addEventListener('message', (event) => {
