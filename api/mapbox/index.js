c3a7419f47c557f43f24acdb5190e0c0c4f23b53
Tran Quoc Anh <tranquocanh3009@gmail.com>
2020-04-11 21:56:41 +0700

    mapbox5

---
+++ api/mapbox/index.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoidHJhbnF1b2NhbmgzMDA5IiwiYSI6ImNrOHZsMHp6ajBkajgzZ282cTUxbXp1dnEifQ.Wu-3h9DOFbunDmHXKzGuIA';
+
+var map = new mapboxgl.Map({
+container: 'map', // container id
+style: './style.json', // stylesheet location
+center: [106.693603, 10.770175], // starting position [lng, lat]
+zoom: 2 // starting zoom
+});
