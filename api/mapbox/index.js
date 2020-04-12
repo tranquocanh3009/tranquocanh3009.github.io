34ae80a82fdae304123c36a2996f8ba7436dd8ed
Tran Quoc Anh <tranquocanh3009@gmail.com>
2020-04-12 21:49:53 +0700

    covid19 mathdro

--- api/mapbox/index.js
+++ api/mapbox/index.js
@@ -2,7 +2,13 @@ mapboxgl.accessToken = 'pk.eyJ1IjoidHJhbnF1b2NhbmgzMDA5IiwiYSI6ImNrOHZsMHp6ajBka
 
 var map = new mapboxgl.Map({
 container: 'map', // container id
-style: './style.json', // stylesheet location
+style: 'style.json', // stylesheet location
 center: [106.693603, 10.770175], // starting position [lng, lat]
 zoom: 2 // starting zoom
 });
+
+
+function draw() {
+    loadImage()
+}
+
